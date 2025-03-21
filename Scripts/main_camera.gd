ab9731d301d846af19cc6708a776e370bb767cf9
habs <brenohabib2013@gmail.com>
2025-03-20 23:29:24 -0300

    testing environments

--- Scripts/main_camera.gd
+++ Scripts/main_camera.gd
@@ -8,12 +8,12 @@ func _ready():
 	global_position = get_parent().global_position
 	_update_screen( cur_screen )
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var parent_screen : Vector2 = ( get_parent().global_position / screen_size ).floor()
 	if not parent_screen.is_equal_approx( cur_screen ):
 		_update_screen( parent_screen )
 
 
-func _update_screen( new_screen : Vector2 ):
+func _update_screen(new_screen : Vector2):
 	cur_screen = new_screen
 	global_position = cur_screen * screen_size + screen_size * 0.5
